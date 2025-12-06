5105628accdc87f5aa90f000d6366d5dcaa06b24
Toewaioo <sawtoewaioo2003@gmail.com>
2025-12-06 22:39:02 +0630

    fixed review and rating#1

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,7 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->api(append: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+           // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             SetLocale::class,
         ]);
 
